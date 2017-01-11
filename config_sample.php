26c44e3219860107c2c87f0df4eb1b45e89a03b7
zigkasg <ziggasg@hotmail.com>
2017-01-11 09:27:27 +0200

    Update config_sample.php

--- config_sample.php
+++ config_sample.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'verify_token' => '',
-    'token' => ''
+    'verify_token' => 'test',
+    'token' => 'EAAFdZAPv0XKsBAO8Vse9y0edZBjeBGjI3Rd5P4G89NwzptHOW1kaBvpwyzutCv2104vzGRZA1laRfVXwQB6AXd3aHUe9oU6VSSryGUi30WzgoZA7Atgu18ScfGBIFSuVLI1qQ6HKZC3WZBfUJt1W8jtPcpMPOus2VZCygVUI0FPNgZDZD'
 ];
